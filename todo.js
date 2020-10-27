d03b756532389119898a448ac1f2929120711a16
mameGG <r.takahashi@april-knights.com>
2020-10-27 21:17:45 +0900

    test2

--- todo.js
+++ todo.js
@@ -8,18 +8,37 @@ const removeTask = removeButton => {
   const targetTask = removeButton.closest('li');
   addTaskTarget.removeChild(targetTask);
 };
+
 const addTask = task => {
   const listItem = document.createElement('li');
   const removeButton = document.createElement('button');
+  const complateButton = document.createElement('button');
+
 	removeButton.innerText = '削除';
   removeButton.addEventListener('click', () => removeTask(removeButton));
-	listItem.innerText = task;
+
+  complateButton.innerText = '完了';
+  complateButton.addEventListener('click', () => complateTask(complateButton));
+
+  listItem.innerText = task;
+  
+  listItem.append(removeButton);
+  addTaskTarget.appendChild(listItem);
+
+  listItem.append(complateButton);
   listItem.append(removeButton);
   addTaskTarget.appendChild(listItem);
 };
+
 addTaskTrigger.addEventListener('click', event => {
-  const task = addTaskValue.Value;
+  const task = addTaskValue.value;
   addTask(task);
   addTaskValue.value = '';
 });
+
+const complateTask = complateButton => {
+  const targetTask = complateButton.closest('li');
+  targetTask.classList.add('isComplate');
+  targetTask.removeChild(complateButton);
+};
 
